5e0ed5288b1599afb9944ca68b289c18c99745bf
Mr7z <2438171792@qq.com>
2018-10-22 00:29:15 +0800

    181022am

---
+++ TestZ.OA.UI.Portal/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZ.OA.UI.Portal
+{
+    public class Class1
+    {
+    }
+}
